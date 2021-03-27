5bd052e440e2054552aa7e95cff182b35172d217
Wagner Rodrigues <wagnersr92@gmail.com>
2021-03-27 00:44:31 -0300

    Conclusão dos desafios

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does no Exists");
+    }
+
+    if (!user.admin) {
+      throw new Error("User does not admin");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,15 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const userProfile = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.json(userProfile);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
